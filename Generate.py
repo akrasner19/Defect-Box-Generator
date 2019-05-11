c15c33955cd8165e91f1137b1ae75b0e1ceb2241
Alex <akrasner19@gmail.com>
2019-05-11 18:16:01 -0400

    Changed generation and added file migration

--- Generate.py
+++ Generate.py
@@ -1,28 +1,35 @@
 import drawSvg as draw
-from Process import *
+from Process import DotGenerator
 import random as rand
 
 #declare process wide, constant variables
 CANVASWIDTH = 300
 CANVASHEIGHT = 300
-DOTCOLOR = '#000000'
-DOTRADIUS = 2
-DOTNUMBER = 100
-SEED = "testingseed1"
+DOTCOLOR = '#000000' #can be implemented as a set to pull from
+DOTRADIUS = 2 #this can also be dynamic
+DOTNUMBER = 100 #add variable dotnumber as a range in future
 BOXCOLOR = 'red'
 BOXTHICKNESS = 4
+SEEDLIST = ["testingseed1",
+	"testingseed2"]
 
-drawing = draw.Drawing(CANVASWIDTH,CANVASHEIGHT,origin='center')
+for SEED in SEEDLIST:
+	drawing = draw.Drawing(CANVASWIDTH,CANVASHEIGHT,origin='center')
 
-rand.seed(SEED)
+	rand.seed(SEED)
 
-dg = DotGenerator(DOTRADIUS,CANVASWIDTH,CANVASHEIGHT,DOTCOLOR,BOXCOLOR,BOXTHICKNESS)
+	dg = DotGenerator(DOTRADIUS,CANVASWIDTH,CANVASHEIGHT,DOTCOLOR,BOXCOLOR,BOXTHICKNESS)
 
-for i in range(DOTNUMBER):
-	drawing.append(dg.generateDot())
+	for i in range(DOTNUMBER):
+		drawing.append(dg.generateDot())
 
-drawing.append(dg.generateBoundingBox())
+	drawing.setPixelScale(1)
+	drawing.saveSvg(SEED + "_defect.svg")
 
-drawing.setPixelScale(2)
-drawing.saveSvg('test.svg')
-#drawing.savePng('test.png')
+	drawing = draw.Drawing(CANVASWIDTH,CANVASHEIGHT,origin='center')
+	
+	drawing.append(dg.generateBoundingBox())
+
+	drawing.setPixelScale(1)
+	drawing.saveSvg(SEED + "_box.svg")
+	#drawing.savePng('test.png')
---
+++ Replace.py
@@ -0,0 +1,28 @@
+import os
+import shutil
+
+REPLACEMENTPATH = 'D:\\Unity Projects\\Symposium Demo Shared SA\\Assets\\'
+
+success = False
+
+while not success:
+    answer = input("Unity MUST BE CLOSED in order for this script to work. Please check to ensure it is closed!\n\nIs Unity closed (y/n)? ")
+    if answer == "y":
+        print("Copying and Overwriting SVG files from this folder to " + REPLACEMENTPATH + "...")
+
+        #find files
+        fileList = os.listdir()
+        svgList = [x for x in fileList if len(x) > 4 and x[-4:] == ".svg"]
+
+        #copy files over
+        for svg in svgList:
+            shutil.copy(svg,REPLACEMENTPATH)
+
+        print("Done!")
+        success = True
+
+    elif answer == "n":
+        success = True
+
+    else:
+        print("Invalid Response")
